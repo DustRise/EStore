2cb6e5c2d406735d67e2c7389116cf877ba4123b
Siva Pandeti <psivah@yahoo.com>
2013-03-26 15:56:02 -0400

    Initial Commit

---
+++ EStoreTests/EStoreTests.h
@@ -0,0 +1,13 @@
+//
+//  EStoreTests.h
+//  EStoreTests
+//
+//  Created by span on 3/26/13.
+//  Copyright (c) 2013 DustRise. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface EStoreTests : SenTestCase
+
+@end
---
+++ EStoreTests/EStoreTests.m
@@ -0,0 +1,32 @@
+//
+//  EStoreTests.m
+//  EStoreTests
+//
+//  Created by span on 3/26/13.
+//  Copyright (c) 2013 DustRise. All rights reserved.
+//
+
+#import "EStoreTests.h"
+
+@implementation EStoreTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in EStoreTests");
+}
+
+@end
